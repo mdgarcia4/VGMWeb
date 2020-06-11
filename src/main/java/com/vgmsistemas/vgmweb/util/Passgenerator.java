6ec9ac60db32f798b22ee93369de909797ff947d
GustavoBravo <gustavorbravo@gmail.com>
2020-06-11 17:45:58 -0300

    saque los parametros de entrada para no hacer un punto de entrada en la ejecucion del proyecto.

--- src/main/java/com/vgmsistemas/vgmweb/util/Passgenerator.java
+++ src/main/java/com/vgmsistemas/vgmweb/util/Passgenerator.java
@@ -4,7 +4,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class Passgenerator {
 
-	public static void main(String[] args) {
+	public static void main() {
+	/*public static void main(String[] args) {*/
 		BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder(4);
         //El String que mandamos al metodo encode es el password que queremos encriptar.
 	System.out.println(bCryptPasswordEncoder.encode("846"));
