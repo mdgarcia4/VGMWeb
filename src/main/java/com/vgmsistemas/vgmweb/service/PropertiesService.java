518e0062251b75c662be9013532abc9d87f0a94e
GustavoBravo <gustavorbravo@gmail.com>
2020-06-23 18:08:27 -0300

    se agrega el token y la expiración para resguardo

--- src/main/java/com/vgmsistemas/vgmweb/service/PropertiesService.java
+++ src/main/java/com/vgmsistemas/vgmweb/service/PropertiesService.java
@@ -6,13 +6,59 @@ import org.springframework.stereotype.Service;
 @Service
 public class PropertiesService {
 	@Value("${contact.apikey}")
-	private String apiKeyGoogle;
+	private String apiKeyGoogle;	
+	
+	@Value("${shopping-cart.urlservicio}")
+	private String urlServicio;
+	
+	@Value("${shopping-cart.token}")
+	private String keyToken;
+	
+	@Value("${shopping-cart.expires_in}")
+	private int expiresIn;
+
 
 	public String getApiKeyGoogle() {
-		return apiKeyGoogle;
+		if(apiKeyGoogle == null) {
+			return "";
+		}else {
+			return apiKeyGoogle;
+		}
 	}
 
 	public void setApiKeyGoogle(String apiKeyGoogle) {
 		this.apiKeyGoogle = apiKeyGoogle;
+	}
+
+	public String getUrlServicio() {
+		if(urlServicio == null) {
+			return "";
+		}else {
+			return urlServicio;
+		}
+	}
+
+	public void setUrlServicio(String urlServicio) {
+		this.urlServicio = urlServicio;
+	}
+
+	public String getKeyToken() {
+		if(keyToken == null) {
+			return "";
+		}else {
+			return keyToken;
+		}
+	}
+
+	public void setKeyToken(String keyToken) {
+		this.keyToken = keyToken;
+	}
+
+	public int getExpiresIn() {		
+		return expiresIn;
+	}
+
+	public void setExpiresIn(int expiresIn) {
+		this.expiresIn = expiresIn;
 	}
 }
