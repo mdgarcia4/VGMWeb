70ea0287f3ba7bb07d6c54cd6324a967e6297081
GustavoBravo <gustavorbravo@gmail.com>
2020-06-23 18:16:57 -0300

    se agrega para menejar los msj desde los servicios rest

---
+++ src/main/java/com/vgmsistemas/vgmweb/util/CodeResult.java
@@ -0,0 +1,15 @@
+package com.vgmsistemas.vgmweb.util;
+
+public class CodeResult {
+    public static final int RESULT_OK = 0;
+	public static final int RESULT_ERROR = 1;
+	public static final int RESULT_MONTO_DISPONIBLE_CERO_RECIBO_BO = 2;	
+	public static final int RESULT_NO_SE_ENCONTRO_UN_COMPROBANTE_EN_CTACTE_RECIBO_BO = 3;
+	public static final int RESULT_ALGUN_COMPROBANTE_TIENE_SALDO_CERO = 4;
+	public static final int RESULT_VENDEDOR_INVALID = 101;
+	public static final int RESULT_PEDIDOS_NO_AUTORIZADO = 102;
+	public static final int RESULT_RECIBO_YA_IMPUTADO = 103;
+	public static final int RESULT_PEDIDO_EXISTENTE = 104; // lo dejo para posterior uso.
+		
+
+}
