e12d227d8c2595f940e7c078aa49d7be4c99f51d
GustavoBravo <gustavorbravo@gmail.com>
2020-06-23 18:17:48 -0300

    se agrega la clase para manejar las rutas a los servicios rest del preventa

---
+++ src/main/java/com/vgmsistemas/vgmweb/util/RutasServicios.java
@@ -0,0 +1,8 @@
+package com.vgmsistemas.vgmweb.util;
+
+public class RutasServicios {
+    public static final String SERVICE_AUTORIZATION = "/oauth/token";
+    public static final String SERVICE_BASE = "/servicios";
+    public static final String SERVICE_DAILY_ORDER = SERVICE_BASE + "/crearPedidoDiario";
+
+}
