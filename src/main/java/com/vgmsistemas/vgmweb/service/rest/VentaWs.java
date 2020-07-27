6000c353d86f5d79a719d9689ee60e2515741caf
GustavoBravo <gustavorbravo@gmail.com>
2020-07-27 13:31:42 -0300

    Se incorpora el manejo de la llamada al servicio del preventa por pedido diario

--- src/main/java/com/vgmsistemas/vgmweb/service/rest/VentaWs.java
+++ src/main/java/com/vgmsistemas/vgmweb/service/rest/VentaWs.java
@@ -1,28 +1,42 @@
 package com.vgmsistemas.vgmweb.service.rest;
 
-import org.springframework.stereotype.Service;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
 import com.vgmsistemas.vgmweb.dto.DailyOrderWsDto;
+import com.vgmsistemas.vgmweb.service.PropertiesService;
+import com.vgmsistemas.vgmweb.util.CodeResult;
+import com.vgmsistemas.vgmweb.util.RutasServicios;
 
+@Component
 public class VentaWs extends GenericWs{
 	
+	@Autowired
+    public VentaWs(PropertiesService property){ 
+    	this.propertyService =  property;
+    	
+		url = propertyService.getUrlServicio(); 	
+    }
+	
 	public int send(DailyOrderWsDto venta) throws Exception{
-		//String peticion = "/createPedido/";
-		//String paramName = "pedido";
-		//metodo = Request.Method.POST;
-
-		//Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSZ").create();
-		//JsonObject jsonPedido = gson.toJsonTree(venta).getAsJsonObject();
-		//Map<String, String> parametros = new HashMap<>();
-		//parametros.put("pedido", jsonPedido.toString());
-		//String response = callWebService(url, webService+peticion, metodo, parametros,/*jsonPedido, paramName, */VOLUMEN_DE_DATOS_MEDIO);
-		//CodeResult.checkCode(Integer.valueOf(response));
-		//try {
-         //   int result = Integer.valueOf(response);
-        //    return result;
-        //} catch (Exception e){
-		//    return RESULT_ERROR;
-        //}
-		return 1;
+		String peticion = RutasServicios.SERVICE_DAILY_ORDER;
+		
+		Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSZ").create();
+		JsonObject jsonPedido = gson.toJsonTree(venta).getAsJsonObject();
+		Map<String, String> parametros = new HashMap<>();
+		parametros.put("pedido", jsonPedido.toString());
+		int response = callWebService(url, peticion, parametros,VOLUMEN_DE_DATOS_MEDIO);
+		
+		try {
+            int result = Integer.valueOf(response);
+            return result;
+        } catch (Exception e){
+		    return CodeResult.RESULT_ERROR;
+        }
 	}
 }
