13d112157396df72ea46b622e222adc1d3fdfab1
GustavoBravo <gustavorbravo@gmail.com>
2020-07-27 12:41:30 -0300

    Se modifico sin hacer nada

--- src/main/java/com/vgmsistemas/vgmweb/LoginSecurityTutorialApplication.java
+++ src/main/java/com/vgmsistemas/vgmweb/LoginSecurityTutorialApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LoginSecurityTutorialApplication {
+public class LoginSecurityTutorialApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(LoginSecurityTutorialApplication.class, args);
