35b5632af30eb1bdc1b1b64b300cc75dc4eda007
GustavoBravo <gustavorbravo@gmail.com>
2020-08-03 17:54:31 -0300

    Se cambia el metodo de paginado

--- src/main/java/com/vgmsistemas/vgmweb/controller/CategoriasController.java
+++ src/main/java/com/vgmsistemas/vgmweb/controller/CategoriasController.java
@@ -32,40 +32,30 @@ import org.springframework.ui.Model;
 @Controller
 @RequestMapping("/categorias")
 public class CategoriasController {
-	
+
 	@Autowired
 	ArticuloService articuloService;
-	
 	@Autowired
 	RubroService rubroService;
-	
 	@Autowired
 	MarcaService marcaService;
-	
 	@Autowired
 	ProveedorService proveedorService;
-	
 	@Autowired
 	ClienteService clienteService;
-	
 	@Autowired
 	ListaPrecioDetalleService listaPrecioDetalleService;
-	
 	@Autowired
 	PropertiesService propertyService;
-	
-		
-	//@GetMapping ("/categorias")
+
 	@GetMapping
 	public String categorias(@RequestParam(defaultValue = "1") Integer page,
-            @RequestParam(defaultValue = "12") Integer size,
-            @RequestParam(defaultValue = "articulo.descripcion") String order,
-            @RequestParam(defaultValue = "0") Long rubro,
-            @RequestParam(defaultValue = "0") Long subrubro,
-            @RequestParam(defaultValue = "0") Long proveedor,
-            @RequestParam(defaultValue = "0") Long marca,
-            Model model) {
-		
+			@RequestParam(defaultValue = "12") Integer size,
+			@RequestParam(defaultValue = "articulo.descripcion") String order,
+			@RequestParam(defaultValue = "0") Long rubro, @RequestParam(defaultValue = "0") Long subrubro,
+			@RequestParam(defaultValue = "0") Long proveedor, @RequestParam(defaultValue = "0") Long marca,
+			Model model) {
+
 		int paginaRecuperar;
 		int paginasTotal;
 		int paginaSiguiente;
@@ -73,89 +63,81 @@ public class CategoriasController {
 		int productoDesde;
 		int productoHasta;
 		long productosTotal;
-		
-		
-		// Obtengo el usuario para poder obtener el cliente relacionado con su lista de precio
-		Authentication auth = SecurityContextHolder
-				.getContext()
-				.getAuthentication();
+
+		// Obtengo el usuario para poder obtener el cliente relacionado con su lista de
+		// precio
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 		UserDetails userDetail = (UserDetails) auth.getPrincipal();
 		String usuario = userDetail.getUsername();
-		
+
 		Cliente cliente = clienteService.getClienteByUsuario(usuario);
-		
-		paginaRecuperar = page - 1; 
-		
-		Page<ListaPrecioDetalle> paginaArticulos = listaPrecioDetalleService.getListaPrecio(cliente.getId().getIdSucursal(),cliente.getListaPrecio().getId(),paginaRecuperar,size,order,rubro,subrubro,proveedor,marca);
-		
-		
+
+		paginaRecuperar = page - 1;
+
+		Page<ListaPrecioDetalle> paginaArticulos = listaPrecioDetalleService.getListaPrecioPorCliente(cliente,
+				paginaRecuperar, size, order, rubro, subrubro, proveedor, marca);
+
 		paginasTotal = paginaArticulos.getTotalPages();
 		productosTotal = paginaArticulos.getTotalElements();
-		
-		List<Integer> paginas = getVectorPaginas(page,paginasTotal);
-		
+
+		List<Integer> paginas = getVectorPaginasLista(page, paginasTotal);
+
 		if (page <= 1) {
 			paginaAnterior = 1;
-		}
-		else {
+		} else {
 			paginaAnterior = page - 1;
 		}
-		
+
 		if (page >= paginasTotal) {
-			paginaSiguiente = page ;
-		}
-		else {
+			paginaSiguiente = page;
+		} else {
 			paginaSiguiente = page + 1;
 		}
-		
+
 		productoDesde = paginaRecuperar * size + 1;
 		productoHasta = paginaRecuperar * size + size;
-				
-		model.addAttribute("paginas",paginas);
+
+		model.addAttribute("paginas", paginas);
 		model.addAttribute("marcas", marcaService.getBySnWeb("S"));
 		model.addAttribute("rubros", rubroService.getBySnWeb("S"));
 		model.addAttribute("proveedores", proveedorService.getBySnWeb("S"));
-		model.addAttribute("preciosArticulos",paginaArticulos);
+		model.addAttribute("preciosArticulos", paginaArticulos);
 		model.addAttribute("paginaAnterior", paginaAnterior);
-		model.addAttribute("paginaActual",page);
-		model.addAttribute("paginaSiguiente",paginaSiguiente);
+		model.addAttribute("paginaActual", page);
+		model.addAttribute("paginaSiguiente", paginaSiguiente);
 		model.addAttribute("productoDesde", productoDesde);
 		model.addAttribute("productoHasta", productoHasta);
 		model.addAttribute("productosTotal", productosTotal);
 		model.addAttribute("preciominimo", 10);
 		model.addAttribute("preciomaximo", 50000);
-		model.addAttribute("nameapp",propertyService.getNameApp());
-		
-		
-		
+		model.addAttribute("nameapp", propertyService.getNameApp());
+
 		return "categorias";
 	}
 	
-	private List<Integer> getVectorPaginas(Integer pagNro, Integer paginasTotal) {
-		int vectorPaginaInial;
-		int vectorPaginaFinal;
-	 	vectorPaginaInial = pagNro / 6;
-		vectorPaginaInial = vectorPaginaInial * 6 + 1;
-		vectorPaginaFinal = vectorPaginaInial + 6;
-		if (vectorPaginaFinal > paginasTotal) {
-			vectorPaginaFinal = paginasTotal;
+	private List<Integer> getVectorPaginasLista(Integer pagVisual, Integer totalPaginas) {
+		int tamaniopagina, pagInicial, pagFinal, cociente, residuo;
+		tamaniopagina = 7;
+		pagInicial = 1;
+		if ((pagVisual / tamaniopagina) >= 1) {
+			cociente = pagVisual / tamaniopagina;
+			residuo = pagVisual % tamaniopagina;
+			pagInicial = (residuo == 0) ? (cociente * tamaniopagina) : (cociente * tamaniopagina + 1);
 		}
-		
-		List<Integer> paginas = IntStream.rangeClosed(vectorPaginaInial, vectorPaginaFinal).boxed().collect(Collectors.toList());
-			
+		pagFinal = ((pagInicial + (tamaniopagina - 1)) > totalPaginas) ? totalPaginas
+				: (pagInicial + (tamaniopagina - 1));
+		List<Integer> paginas = IntStream.rangeClosed(pagInicial, pagFinal).boxed().collect(Collectors.toList());
+
 		return paginas;
 	}
-	
-	
-	//@GetMapping("/categorias/search" , method = RequestMethod.POST)
-		//@PostMapping(value = "/categorias",method = RequestMethod.POST)
-		@PostMapping
-		public String categoriasSearch(String search,  Model model, @PageableDefault(page = 0, size = 9) Pageable pageable) {
-			model.addAttribute("marcas", marcaService.getBySnWeb("S"));
-			model.addAttribute("rubros", rubroService.getBySnWeb("S"));
-			model.addAttribute("proveedores", proveedorService.getBySnWeb("S"));
-			model.addAttribute("articulos",articuloService.searchByDescripcion(search, pageable));
-			
-			return "categorias";
-		}
+
+	@PostMapping
+	public String categoriasSearch(String search, Model model, @PageableDefault(page = 0, size = 9) Pageable pageable) {
+		model.addAttribute("marcas", marcaService.getBySnWeb("S"));
+		model.addAttribute("rubros", rubroService.getBySnWeb("S"));
+		model.addAttribute("proveedores", proveedorService.getBySnWeb("S"));
+		model.addAttribute("articulos", articuloService.searchByDescripcion(search, pageable));
+
+		return "categorias";
+	}
 }
