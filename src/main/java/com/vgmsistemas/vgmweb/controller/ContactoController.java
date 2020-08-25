439679995a375fa38c64b73a825788e434db298c
GustavoBravo <gustavorbravo@gmail.com>
2020-08-24 21:32:59 -0300

    Se mejora formato, se limpia el código, se agrega el tramiento de exceptiones y logs

--- src/main/java/com/vgmsistemas/vgmweb/controller/ContactoController.java
+++ src/main/java/com/vgmsistemas/vgmweb/controller/ContactoController.java
@@ -2,6 +2,8 @@ package com.vgmsistemas.vgmweb.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,32 +17,36 @@ import com.vgmsistemas.vgmweb.service.PropertiesService;
 public class ContactoController {
 	@Autowired
 	SucursalService sucursalService;
-
 	@Autowired
 	PropertiesService prepertyService;
-	
-	@GetMapping("/contacto" )
+	static Logger logger = LoggerFactory.getLogger(ContactoController.class);
+
+	@GetMapping("/contacto")
 	public String contacto(Model model) {
 		Float latitud = -29.704071F;
 		Float longitud = -57.1297873F;
 		String infoWindowDesc = "Casa Central";
-		List<Sucursal> sucursales;	
-		sucursales = sucursalService.getAll();
-		for(Sucursal suc:sucursales)
-		{
-			if(suc.getId() == suc.getSucActivaEmpresa())
-			{
-				latitud = suc.getLatitud();
-				longitud = suc.getLongitud();
-				infoWindowDesc = suc.getDescripcion();
+		try {
+			List<Sucursal> sucursales;
+			sucursales = sucursalService.getAll();
+			for (Sucursal suc : sucursales) {
+				if (suc.getId() == suc.getSucActivaEmpresa()) {
+					latitud = suc.getLatitud();
+					longitud = suc.getLongitud();
+					infoWindowDesc = suc.getDescripcion();
+				}
 			}
+			model.addAttribute("latitud", latitud);
+			model.addAttribute("longitud", longitud);
+			model.addAttribute("infoWindowDescripcion", infoWindowDesc);
+			model.addAttribute("sucursales", sucursales);
+			model.addAttribute("apiKey", prepertyService.getApiKeyGoogle());
+			model.addAttribute("nameapp", prepertyService.getNameApp());
+			return "contacto";
+		} catch (Exception e) {
+			logger.error("Error inesperado en clase ContactoController-Página: contacto. " + e.getStackTrace());
+			return "error";
 		}
-		model.addAttribute("latitud", latitud);
-		model.addAttribute("longitud",longitud);
-		model.addAttribute("infoWindowDescripcion",infoWindowDesc);
-		model.addAttribute("sucursales",sucursales );
-		model.addAttribute("apiKey",prepertyService.getApiKeyGoogle());
-		model.addAttribute("nameapp",prepertyService.getNameApp());
-		return "contacto";
+
 	}
 }
--- src/main/java/com/vgmsistemas/vgmweb/controller/ShoppingCartController.java
+++ src/main/java/com/vgmsistemas/vgmweb/controller/ShoppingCartController.java
@@ -2,7 +2,12 @@ package com.vgmsistemas.vgmweb.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,31 +17,54 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.vgmsistemas.vgmweb.dto.DetailViewWsDto;
+import com.vgmsistemas.vgmweb.entity.Clientes;
+import com.vgmsistemas.vgmweb.service.ClienteService;
 import com.vgmsistemas.vgmweb.service.PropertiesService;
 import com.vgmsistemas.vgmweb.service.VentaService;
 
 @Controller
-@RequestMapping({"/shopping-cart","/shopping-cart.html","/shopping-cart.htm"})
+@RequestMapping({ "/shopping-cart", "/shopping-cart.html", "/shopping-cart.htm" })
 public class ShoppingCartController {
 	@Autowired
 	VentaService vtaService;
-
 	@Autowired
-	PropertiesService prepertyService;	
+	PropertiesService prepertyService;
+	@Autowired
+	ClienteService clienteService;
+	static Logger logger = LoggerFactory.getLogger(ShoppingCartController.class);
+
 	@GetMapping
-	public String getShoppingCar(Model model) {		
-		model.addAttribute("token","nada");
-		model.addAttribute("nameapp",prepertyService.getNameApp());
-		return "shopping-cart";
+	public String getShoppingCar(Model model) {
+		try {
+			// Obtengo el usuario
+			Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+			UserDetails userDetail = (UserDetails) auth.getPrincipal();
+			String usuario = userDetail.getUsername();
+			Clientes clientesComerciosPorUsuario = clienteService.getAllClientesByUsuario(usuario);
+						
+			model.addAttribute("comercios",clientesComerciosPorUsuario.getClientes());
+			model.addAttribute("token", "");
+			model.addAttribute("nameapp", prepertyService.getNameApp());
+			return "shopping-cart";
+		} catch (Exception e) {
+
+			model.addAttribute("token", "");
+			model.addAttribute("nameapp", prepertyService.getNameApp());
+			return "shopping-cart";
+		}
 	}
-	
+
 	@PostMapping
 	public @ResponseBody String checkin(@RequestBody DetailViewWsDto detallePedido, HttpServletRequest request) {
 		if (detallePedido == null) {
 			return "el servidor no pudo interpretar la solicitud dada una sintaxis inválida.";
-		}else {
-			return vtaService.generarVenta(detallePedido);
+		} else {
+			try {
+				return vtaService.generarVenta(detallePedido);
+			} catch (Exception e) {
+				logger.error("Error inesperado en clase ContactoController-Página: contacto. " + e.getStackTrace());
+				return "error";
+			}
 		}
 	}
 }
-
